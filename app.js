5ee237849fb7324f7f958a8737bf3bad7f1c7d25
Nayanajith Priyasad <nayanajithpriyasad@gmail.com>
2020-04-25 14:18:41 +0530

    #2-5_Data Binding

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 new Vue({
     el:'#app',
     data:{
-       title : 'Becoming a Vue Ninja' 
+       title : 'Becoming a Vue Ninja',
+       name : 'Ryu',
+       url:'yotube.com',
+       classes:['one','two']
     },
     methods: {
-        greet(){
-            return 'Hello there'
+        greet(time){
+            // return 'Hello and good ${time}, ${this.name}';
+            return 'Hello and good '+time+' ,'+this.name+''
         }
     }
 })
